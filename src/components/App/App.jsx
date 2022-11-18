40fc86fd38178a27a2504eaa61fddeaa280f3563
Stanislav Karmanov <stanislav.karmanov1@gmail.com>
2022-11-18 13:28:28 +0200

    fix styles

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,16 +6,11 @@ import { ContactList } from '../ContactList/ContactList';
 import { Filter } from '../Filter/Filter';
 import { Box } from 'components/Box/Box';
 import { Title, SupTitle, Message } from './App.styled';
+import data from '../../data/data.json';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-
+    contacts: data,
     filter: '',
   };
   addContact = (name, number) => {
